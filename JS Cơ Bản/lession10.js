7a17f88239b7128ef201027478d4fdfb31cb953a
Tiến Đạt <47631594+ntdat104@users.noreply.github.com>
2020-03-13 13:04:23 +0700

    update

--- JS Cơ Bản/lession10.js
+++ JS Cơ Bản/lession10.js
@@ -58,8 +58,33 @@ tom
   .eat(m2)
   .eat(m3); //* Gọi nhiều lần trong 1 object bởi vì return this.
 
-  console.log("Tom = ", tom);
-
+console.log("Tom = ", tom);
 
 //! Prototypes in JavaScript - Nguyên mẫu trong javascript
+function Mouse(color, weight) {
+  this.type = "mouse";
+  this.color = color;
+  this.weight = weight;
+  /*
+   * this.sleep = function(){
+   *     console.log("Sleeping ...");
+   * }
+   */
+}
+console.log(Mouse);
+console.log(Mouse.prototype); //! Prototypes = thuộc tính.
+console.log(Mouse.prototype.constructor);
+console.log(Mouse.prototype.constructor === Mouse);
 
+Mouse.prototype.sleep = function() {
+  //* dùng cách này tiết kiệm bộ nhớ hơn
+  console.log(this.color + " Sleeping ...");
+};
+var jerry = new Mouse("orange", 20);
+console.log("jerry = ", jerry);
+jerry.sleep();
+
+var mickey = new Mouse("white", 10);
+mickey.sleep();
+
+console.log(jerry.sleep === mickey.sleep)
