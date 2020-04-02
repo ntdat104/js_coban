a88e6b3f325581efaf2cf4e5b24f25b1a093e9ef
Tiến Đạt <47631594+ntdat104@users.noreply.github.com>
2020-04-02 14:32:33 +0700

    add lession11

---
+++ lession/lession11/index.js
@@ -0,0 +1,13 @@
+var Mouse = require("./mouse");
+var Cat = require("./cat");
+
+var mickey = new Mouse("Black");
+var jerry = new Mouse("orange");
+
+console.log(mickey);
+console.log(jerry);
+
+var tom = new Cat();
+tom.eat(mickey);
+tom.eat(jerry);
+console.log(tom);
