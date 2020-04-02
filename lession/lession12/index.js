4eb6cd7464b9ae8a54606134d1bf78889d797650
Tiến Đạt <47631594+ntdat104@users.noreply.github.com>
2020-04-02 14:49:19 +0700

    add lession12

---
+++ lession/lession12/index.js
@@ -0,0 +1,8 @@
+var fs = require("fs");
+
+var readFile = fs.readFileSync("./song.txt", { encoding:"UTF8" });
+console.log(readFile);
+
+fs.writeFileSync("./song-2.txt", "I am a coder");
+var readFile2 = fs.readFileSync("./song-2.txt", { encoding:"UTF8" });
+console.log(readFile2);
